ace6133a89fe8197306c0fc4d69745919d66fd4a
maksli <Max2004lee@mail.ru>
2023-11-01 14:02:04 +0300

    init

---
+++ Avto/Avto.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Avto
+{
+	public class Auto
+	{
+        private int Speed;
+        private int Petrol;
+        private int PetrolUser;
+        private int Distance;
+
+        public Auto(int Speed, int Petrol, int PetrolUser, int Distance, int HourVar)
+		{
+            this.Speed = Speed;
+            this.Petrol = Petrol;
+            this.PetrolUser = PetrolUser;
+            this.Distance = Distance;
+        }
+
+        public void ToBegin()
+        {
+            Random rnd = new Random();
+            Distance = rnd.Next(1000);
+            Petrol = 100;
+        }
+
+		public void Drive()
+        {
+            Speed = 50;
+            Console.WriteLine($"Скорость: {Speed}");
+        }
+
+        public void SpeedUp()
+        {
+            Speed = Speed + 10;
+            Console.WriteLine($"Скорость: {Speed}");
+        }
+
+        public void SlowDown()
+        {
+            Speed = 0;
+            Console.WriteLine($"Скорость: {Speed}");
+        }
+
+        public void Refuel()
+        {
+            Console.WriteLine("Введите количество бензина: ");
+            PetrolUser = int.Parse(Console.ReadLine());
+            Petrol = PetrolUser + Petrol;
+            if (Petrol > 100)
+            {
+                Console.WriteLine("Ваш бак переполнен!!!");
+            }
+        }
+
+        public void Information()
+        {
+            Distance = (Distance - (Distance % 10));
+            Console.WriteLine($"Остаток бензина: {Petrol}");
+            Console.WriteLine($"Растояние которое нужно проехать: {Distance}");
+        }
+
+        public void Hour()
+        {
+            if (Speed == 50)
+            {
+                Distance = Distance - Speed;
+                Petrol = Petrol - 10;
+            }
+            else if (Speed == 60)
+            {
+                Distance = Distance - Speed;
+                Petrol = Petrol - 12;
+            }
+            else if (Speed == 70)
+            {
+                Distance = Distance - Speed;
+                Petrol = Petrol - 14;
+            }
+            else if (Speed == 80)
+            {
+                Distance = Distance - Speed;
+                Petrol = Petrol - 16;
+            }
+            else if (Speed == 90)
+            {
+                Distance = Distance - Speed;
+                Petrol = Petrol - 18;
+            }
+            else if (Speed == 100)
+            {
+                Distance = Distance - Speed;
+                Petrol = Petrol - 20;
+            }
+            else
+            {
+                Console.WriteLine("Вы превысили максимальную скорость.");
+            }
+            Console.WriteLine("Прошел один час, посмотрите информацию");
+        }
+    }
+}
---
+++ Avto/Program.cs
@@ -0,0 +1,49 @@
+﻿using Avto;
+
+class Program
+{
+    static void Main()
+    {
+        Auto Untitled = new Auto(0, 0, 0, 0, 0); //Создание объекта на основе класса
+
+        Untitled.ToBegin();
+
+        Console.WriteLine("Нажмите 1 чтобы поехать");
+        Console.WriteLine("Нажмите 2 чтобы ускориться");
+        Console.WriteLine("Нажмите 3 чтобы затормозить");
+        Console.WriteLine("Нажмите 4 чтобы запрвиться");
+        Console.WriteLine("Нажмите 5 чтобы посмотреть информацию");
+        Console.WriteLine("Нажмите 6 чтобы прошел час");
+
+        while (true)
+        {
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+            switch (keyInfo.Key)
+            {
+                case ConsoleKey.D1:
+                Untitled.Drive(); //Поехать
+                break;
+
+                case ConsoleKey.D2:
+                Untitled.SpeedUp();     //Ускорение
+                break;
+                    
+                case ConsoleKey.D3:
+                Untitled.SlowDown(); //Затормозить
+                break;
+
+                case ConsoleKey.D4:
+                Untitled.Refuel(); // Заправиться
+                break;
+
+                case ConsoleKey.D5:
+                Untitled.Information(); //Информация
+                break;
+
+                case ConsoleKey.D6:
+                Untitled.Hour(); //Час
+                break;
+            }
+        }
+    }
+}
